004774abe86041d056a497e57c9d7c1bfe8bca14
Yan Malashonak <yan.malashonak@gmail.com>
2023-06-27 17:26:14 +0300

    init: remove karma+jasmine & install jest

---
+++ jest.config.ts
@@ -0,0 +1,12 @@
+import type { Config } from 'jest';
+
+const jestConfig: Config = {
+  preset: 'jest-preset-angular',
+  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+  moduleDirectories: ['node_modules', '<rootDir/>/src'],
+  transformIgnorePatterns: ['node_modules/(?!@angular|rjxs|@ngrx)'],
+  collectCoverage: true,
+  coverageDirectory: '<rootDir>/coverage',
+};
+
+export default jestConfig;
