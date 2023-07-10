5f0654fbbea9054f3c8dac997273bb668278c4ec
Yan Malashonak <yan.malashonak@gmail.com>
2023-07-10 17:54:10 +0300

    feat: implement nav tabs

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatTabsModule } from '@angular/material/tabs';
 
 import { AuthRoutingModule } from './auth-routing.module';
 import { AuthPageComponent } from './pages/auth-page/auth-page.component';
@@ -8,6 +9,6 @@ import { SignupFormComponent } from './forms/signup-form/signup-form.component';
 
 @NgModule({
   declarations: [AuthPageComponent, LoginFormComponent, SignupFormComponent],
-  imports: [CommonModule, AuthRoutingModule],
+  imports: [CommonModule, AuthRoutingModule, MatTabsModule],
 })
 export class AuthModule {}
--- src/app/auth/pages/auth-page/auth-page.component.ts
+++ src/app/auth/pages/auth-page/auth-page.component.ts
@@ -1,8 +1,24 @@
 import { Component } from '@angular/core';
 
+interface NavTab {
+  link: string;
+  label: string;
+}
+
 @Component({
   selector: 'app-auth-page',
   templateUrl: './auth-page.component.html',
   styleUrls: ['./auth-page.component.scss'],
 })
-export class AuthPageComponent {}
+export class AuthPageComponent {
+  tabs: NavTab[] = [
+    { link: 'login', label: 'Log in' },
+    { link: 'signup', label: 'Sign up' },
+  ];
+
+  activeTab = this.tabs[0];
+
+  onTabSelect(tab: NavTab) {
+    this.activeTab = tab;
+  }
+}
