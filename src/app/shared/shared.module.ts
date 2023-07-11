71cf6b8a1b15a2e2547c6dc0fd2060b66f82c1fa
Yan Malashonak <yan.malashonak@gmail.com>
2023-07-11 15:54:35 +0300

    feat(shared/pipes): implement split-camel-case pipe

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { SentenceCasePipe } from './pipes/sentence-case/sentence-case.pipe';
+import { SplitCamelCasePipe } from './pipes/split-camel-case/split-camel-case.pipe';
 
 @NgModule({
-  declarations: [SentenceCasePipe],
+  declarations: [SentenceCasePipe, SplitCamelCasePipe],
   imports: [CommonModule],
 })
 export class SharedModule {}
