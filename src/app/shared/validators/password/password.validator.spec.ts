e4bcf911ce8e8d7b9c050dcc86767ab27abaf4d3
Yan Malashonak <yan.malashonak@gmail.com>
2023-07-11 15:15:35 +0300

    feat(shared/validators): implement password validator

--- src/app/shared/validators/password/password.validator.spec.ts
+++ src/app/shared/validators/password/password.validator.spec.ts
@@ -1,7 +1,50 @@
+import { FormControl, FormGroup } from '@angular/forms';
 import { PasswordValidator } from './password.validator';
 
-describe('Password', () => {
-  it('should create an instance', () => {
-    expect(new PasswordValidator()).toBeTruthy();
+describe('Password validator', () => {
+  let form: FormGroup;
+  const password = 'password';
+  const passwordConfirm = 'passwordConfirm';
+
+  beforeEach(() => {
+    form = new FormGroup({
+      [password]: new FormControl(),
+      [passwordConfirm]: new FormControl(),
+    });
+  });
+
+  it('should allow same passwords', () => {
+    const passwordField = form.get(password)!;
+    const passwordConfirmField = form.get(passwordConfirm)!;
+
+    passwordField.setValue('12345');
+    passwordConfirmField.setValue('12345');
+
+    const passwordMatchValidator = PasswordValidator.match(
+      password,
+      passwordConfirm
+    );
+
+    expect(passwordMatchValidator(form)).toBeNull();
+  });
+
+  it('should reject differing passwords', () => {
+    const passwordField = form.get(password)!;
+    const passwordConfirmField = form.get(passwordConfirm)!;
+
+    passwordField.setValue('12345');
+    passwordConfirmField.setValue('abcdef');
+
+    const passwordMatchValidator = PasswordValidator.match(
+      password,
+      passwordConfirm
+    );
+
+    expect(passwordMatchValidator(form)).toEqual({
+      passwordsMismatch: {
+        password: passwordField.value,
+        passwordConfirm: passwordConfirmField.value,
+      },
+    });
   });
 });
--- src/app/shared/validators/password/password.validator.ts
+++ src/app/shared/validators/password/password.validator.ts
@@ -6,17 +6,17 @@ export class PasswordValidator {
     passwordConfirmFieldName: string
   ): ValidatorFn {
     return (control: AbstractControl): ValidationErrors | null => {
-      const password = control.get(passwordFieldName);
-      const passwordConfirm = control.get(passwordConfirmFieldName);
+      const password = control.get(passwordFieldName)?.value;
+      const passwordConfirm = control.get(passwordConfirmFieldName)?.value;
 
       return password === passwordConfirm
-        ? {
+        ? null
+        : {
             passwordsMismatch: {
               password,
               passwordConfirm,
             },
-          }
-        : null;
+          };
     };
   }
 }
