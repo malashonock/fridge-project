9afc9f20e644d1c4e8a965d5bafbba1712847893
Yan Malashonak <yan.malashonak@gmail.com>
2023-07-11 10:09:08 +0300

    fix(auth-page/spec): add missing dependencies

--- src/app/auth/pages/auth-page/auth-page.component.spec.ts
+++ src/app/auth/pages/auth-page/auth-page.component.spec.ts
@@ -1,4 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { CommonModule } from '@angular/common';
+import { RouterTestingModule } from '@angular/router/testing';
+import { MatTabsModule } from '@angular/material/tabs';
 
 import { AuthPageComponent } from './auth-page.component';
 
@@ -8,7 +11,8 @@ describe('AuthPageComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [],
+      declarations: [AuthPageComponent],
+      imports: [CommonModule, RouterTestingModule, MatTabsModule],
     }).compileComponents();
 
     fixture = TestBed.createComponent(AuthPageComponent);
