e9f0e3a8cf1378f311abd6b5650523b4b0ba1a52
Yan Malashonak <yan.malashonak@gmail.com>
2023-07-11 18:00:37 +0300

    fix(auth/tests): add missing imports to test bed for signup form

--- src/app/auth/forms/signup-form/signup-form.component.spec.ts
+++ src/app/auth/forms/signup-form/signup-form.component.spec.ts
@@ -1,6 +1,19 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatTabsModule } from '@angular/material/tabs';
+import { AuthRoutingModule } from 'app/auth/auth-routing.module';
+import { SharedModule } from 'app/shared/shared.module';
 
 import { SignupFormComponent } from './signup-form.component';
+import { SentenceCasePipe } from 'app/shared/pipes/sentence-case/sentence-case.pipe';
+import { SplitCamelCasePipe } from 'app/shared/pipes/split-camel-case/split-camel-case.pipe';
+import { RouterTestingModule } from '@angular/router/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('SignupFormComponent', () => {
   let component: SignupFormComponent;
@@ -9,6 +22,20 @@ describe('SignupFormComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [SignupFormComponent],
+      imports: [
+        BrowserAnimationsModule,
+        SharedModule,
+        ReactiveFormsModule,
+        AuthRoutingModule,
+        MatTabsModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatSelectModule,
+        MatButtonModule,
+        MatIconModule,
+        RouterTestingModule,
+      ],
+      providers: [SentenceCasePipe, SplitCamelCasePipe],
     }).compileComponents();
 
     fixture = TestBed.createComponent(SignupFormComponent);
