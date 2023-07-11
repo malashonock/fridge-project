f591a73275bcd39f8f05186d2cc2059f0e0f9cc7
Yan Malashonak <yan.malashonak@gmail.com>
2023-07-11 15:50:54 +0300

    feat(shared/pipes): implement sentence-case pipe

---
+++ src/app/shared/pipes/sentence-case/sentence-case.pipe.ts
@@ -0,0 +1,14 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'sentenceCase',
+})
+export class SentenceCasePipe implements PipeTransform {
+  transform(text: string): string {
+    if (text.length === 0) {
+      return '';
+    }
+
+    return text.charAt(0).toUpperCase() + text.slice(1).toLowerCase();
+  }
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SentenceCasePipe } from './pipes/sentence-case/sentence-case.pipe';
 
 @NgModule({
-  declarations: [],
+  declarations: [SentenceCasePipe],
   imports: [CommonModule],
 })
 export class SharedModule {}
