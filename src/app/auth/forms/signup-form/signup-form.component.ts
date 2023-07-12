ce4d38efa0d54c9e9ff2cc679ff3ae5c3c91463d
Yan Malashonak <yan.malashonak@gmail.com>
2023-07-12 10:31:04 +0300

    refactor(signup-form): move implementation to shared FormBaseComponent class

--- src/app/auth/forms/signup-form/signup-form.component.ts
+++ src/app/auth/forms/signup-form/signup-form.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
-import { SignupCredentials } from 'app/auth/models/signup.model';
+import { FormBaseComponent } from 'app/shared/components/form-base/form-base.component';
 import { UserRole } from 'app/shared/models/user/user-role.model';
 import { SentenceCasePipe } from 'app/shared/pipes/sentence-case/sentence-case.pipe';
 import { SplitCamelCasePipe } from 'app/shared/pipes/split-camel-case/split-camel-case.pipe';
@@ -13,101 +13,28 @@ import { PasswordValidator } from 'app/shared/validators/password/password.valid
   templateUrl: './signup-form.component.html',
   styleUrls: ['./signup-form.component.scss'],
 })
-export class SignupFormComponent {
-  initialValues = {
-    userName: '',
-    email: '',
-    role: null,
-    password: '',
-    passwordConfirm: '',
-  };
-
-  form: FormGroup = this.formBuilder.group(
-    {
-      userName: [
-        this.initialValues.userName,
-        [Validators.required, Validators.minLength(2)],
-      ],
-      email: [
-        this.initialValues.email,
-        [Validators.required, EmailValidator.valid],
-      ],
-      role: [this.initialValues.role as UserRole | null, [Validators.required]],
-      password: [this.initialValues.password, [Validators.required]],
-      passwordConfirm: [
-        this.initialValues.passwordConfirm,
-        [Validators.required],
-      ],
-    },
-    {
-      validators: [PasswordValidator.match('password', 'passwordConfirm')],
-    }
-  );
-
+export class SignupFormComponent extends FormBaseComponent {
   constructor(
-    private formBuilder: FormBuilder,
-    private sentenceCasePipe: SentenceCasePipe,
-    private splitCamelCasePipe: SplitCamelCasePipe
-  ) {}
-
-  getFieldErrorMessage(fieldName: keyof SignupCredentials): string | null {
-    const fieldErrors = this.form.controls[fieldName].errors;
-
-    if (!fieldErrors) {
-      return null;
-    }
-
-    const fieldNameFormatted = this.sentenceCasePipe.transform(
-      this.splitCamelCasePipe.transform(fieldName)
+    formBuilder: FormBuilder,
+    sentenceCasePipe: SentenceCasePipe,
+    splitCamelCasePipe: SplitCamelCasePipe
+  ) {
+    // Instantiate base class
+    super(
+      {
+        userName: ['', [Validators.required, Validators.minLength(2)]],
+        email: ['', [Validators.required, EmailValidator.valid]],
+        role: [null as UserRole | null, [Validators.required]],
+        password: ['', [Validators.required]],
+        passwordConfirm: ['', [Validators.required]],
+      },
+      {
+        validators: [PasswordValidator.match('password', 'passwordConfirm')],
+      },
+      (form: FormGroup): void => console.log(form.value),
+      formBuilder,
+      sentenceCasePipe,
+      splitCamelCasePipe
     );
-
-    if (fieldErrors?.['required']) {
-      return `${fieldNameFormatted} is required`;
-    }
-
-    if (fieldErrors?.['minlength']) {
-      const { requiredLength } = fieldErrors['minlength'];
-      return `${fieldNameFormatted} should be no shorter than ${requiredLength} symbols`;
-    }
-
-    if (fieldErrors?.['invalidEmail']) {
-      return `${fieldNameFormatted} is not a valid email`;
-    }
-
-    return `${fieldNameFormatted} value is not valid`;
-  }
-
-  getFormErrorMessage(): string | null {
-    const formErrors = this.form.errors;
-
-    if (!formErrors) {
-      return null;
-    }
-
-    if (formErrors?.['passwordsMismatch']) {
-      return `Passwords don't match`;
-    }
-
-    return `Form is not valid`;
-  }
-
-  reset(): void {
-    this.form.reset(this.initialValues);
-
-    // Clear error state for each control
-    Object.keys(this.form.controls).forEach((key): void => {
-      this.form.controls[key].setErrors(null);
-    });
-  }
-
-  onSubmit(): void {
-    if (this.form.invalid) {
-      return;
-    }
-
-    // TODO: dispatch login event to store
-    console.log(this.form.value);
-
-    this.reset();
   }
 }
