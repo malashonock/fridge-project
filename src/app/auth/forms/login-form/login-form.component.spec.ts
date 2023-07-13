414d3e9a87a2c7494dfc3045de489b5e57feabf8
Yan Malashonak <yan.malashonak@gmail.com>
2023-07-13 17:18:55 +0300

    feat(auth/components): implement login-form component

--- src/app/auth/forms/login-form/login-form.component.spec.ts
+++ src/app/auth/forms/login-form/login-form.component.spec.ts
@@ -1,6 +1,14 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { SharedModule } from 'app/shared/shared.module';
 
 import { LoginFormComponent } from './login-form.component';
+import { SentenceCasePipe } from 'app/shared/pipes/sentence-case/sentence-case.pipe';
+import { SplitCamelCasePipe } from 'app/shared/pipes/split-camel-case/split-camel-case.pipe';
+import { RouterTestingModule } from '@angular/router/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('LoginFormComponent', () => {
   let component: LoginFormComponent;
@@ -9,6 +17,15 @@ describe('LoginFormComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [LoginFormComponent],
+      imports: [
+        BrowserAnimationsModule,
+        SharedModule,
+        ReactiveFormsModule,
+        MatButtonModule,
+        MatIconModule,
+        RouterTestingModule,
+      ],
+      providers: [SentenceCasePipe, SplitCamelCasePipe],
     }).compileComponents();
 
     fixture = TestBed.createComponent(LoginFormComponent);
--- src/app/auth/forms/login-form/login-form.component.ts
+++ src/app/auth/forms/login-form/login-form.component.ts
@@ -1,8 +1,32 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import { FormBaseComponent } from 'app/shared/components/form-base/form-base.component';
+import { SentenceCasePipe } from 'app/shared/pipes/sentence-case/sentence-case.pipe';
+import { SplitCamelCasePipe } from 'app/shared/pipes/split-camel-case/split-camel-case.pipe';
 
 @Component({
   selector: 'app-login-form',
   templateUrl: './login-form.component.html',
   styleUrls: ['./login-form.component.scss'],
 })
-export class LoginFormComponent {}
+export class LoginFormComponent extends FormBaseComponent {
+  constructor(
+    formBuilder: FormBuilder,
+    sentenceCasePipe: SentenceCasePipe,
+    splitCamelCasePipe: SplitCamelCasePipe
+  ) {
+    // Instantiate base class
+    super(
+      {
+        userName: ['', [Validators.required, Validators.minLength(2)]],
+        password: ['', [Validators.required]],
+      },
+      undefined,
+      (form: FormGroup): void => console.log(form.value),
+      formBuilder,
+      sentenceCasePipe,
+      splitCamelCasePipe
+    );
+  }
+}
