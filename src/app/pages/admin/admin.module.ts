2bb5a5445ac1a3aeb79b45666cfe4ee905399b71
Yan Malashonak <yan.malashonak@gmail.com>
2023-07-19 11:53:10 +0300

    feat(admin): generate admin module

---
+++ src/app/pages/admin/admin.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+
+import { SharedModule } from 'app/shared/shared.module';
+import { AdminRoutingModule } from './admin-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [SharedModule, AdminRoutingModule],
+})
+export class AdminModule {}
