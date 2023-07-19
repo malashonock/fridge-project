51afb3ab44cdcbd2bd555c6119de34f7ec16557b
Yan Malashonak <yan.malashonak@gmail.com>
2023-07-19 12:15:10 +0300

    feat(admin/components): scaffold basic admin-page

--- src/app/pages/admin/admin.module.ts
+++ src/app/pages/admin/admin.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 
 import { SharedModule } from 'app/shared/shared.module';
 import { AdminRoutingModule } from './admin-routing.module';
+import { AdminPageComponent } from './components/admin-page/admin-page.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [AdminPageComponent],
+  exports: [AdminPageComponent],
   imports: [SharedModule, AdminRoutingModule],
 })
 export class AdminModule {}
