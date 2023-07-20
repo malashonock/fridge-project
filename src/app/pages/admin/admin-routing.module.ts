dbea1ef85e542d500e2a32288f3d2d6dbe6539d7
Yan Malashonak <yan.malashonak@gmail.com>
2023-07-20 17:14:52 +0300

    feat(admin/components): scaffold index page

--- src/app/pages/admin/admin-routing.module.ts
+++ src/app/pages/admin/admin-routing.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
 import { AdminPageComponent } from './components/admin-page/admin-page.component';
+import { IndexComponent } from './components/index/index.component';
 
 const routes: Routes = [
   {
     path: '',
     component: AdminPageComponent,
+    children: [
+      {
+        path: '',
+        pathMatch: 'full',
+        component: IndexComponent,
+      },
+    ],
   },
 ];
 
