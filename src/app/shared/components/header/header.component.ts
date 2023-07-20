7f0050eb4835a85bdbf997d1a1717eeffc1a811e
Yan Malashonak <yan.malashonak@gmail.com>
2023-07-20 17:09:47 +0300

    feat(shared/components/header): add burger-button to header component

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,8 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  @Input() compactWidthMode = false;
+
+  private _showMobileMenu = false;
+
+  get showMobileMenu(): boolean {
+    return this._showMobileMenu;
+  }
+
+  @Input() set showMobileMenu(value: boolean) {
+    this._showMobileMenu = value;
+    this.showMobileMenuChange.emit(value);
+  }
+
+  @Output() showMobileMenuChange = new EventEmitter<boolean>();
+}
