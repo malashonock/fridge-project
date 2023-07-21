59de16fe261e4368d6da7fa347619eb9d2a45e43
Yan Malashonak <yan.malashonak@gmail.com>
2023-07-21 13:02:56 +0300

    refactor(admin/components/admin-page): leverage mobile-menu directive

--- src/app/pages/admin/components/admin-page/admin-page.component.ts
+++ src/app/pages/admin/components/admin-page/admin-page.component.ts
@@ -1,15 +1,6 @@
-import { Component, ViewChild } from '@angular/core';
-import {} from '@angular/cdk/layout';
-import { Observable, map } from 'rxjs';
-import { MatDrawerMode, MatSidenav } from '@angular/material/sidenav';
-import { Store } from '@ngrx/store';
+import { Component } from '@angular/core';
 
 import { MenuItemConfig } from 'app/shared/components/menu-item/menu-item.component';
-import {
-  selectMobileMode,
-  selectShowSideMenu,
-} from 'app/state/ui/ui.selectors';
-import { UiActions } from 'app/state/ui/ui.actions';
 import { MobilePageDirective } from 'app/shared/directives/mobile/page/mobile-page.directive';
 
 @Component({
@@ -25,29 +16,7 @@ export class AdminPageComponent {
     { text: 'Map', href: 'map', matIconCode: 'map' },
   ];
 
-  mobileMode$: Observable<boolean>;
-  showSideMenu$: Observable<boolean>;
-  sideMenuMode$: Observable<MatDrawerMode>;
-
-  @ViewChild('sideMenu') sideMenu?: MatSidenav;
-
-  constructor(private store: Store) {
-    this.mobileMode$ = this.store.select(selectMobileMode);
-    this.showSideMenu$ = this.store.select(selectShowSideMenu);
-    this.sideMenuMode$ = this.mobileMode$.pipe(
-      map(this.getSideMenuMode.bind(this))
-    );
-  }
-
-  toggleSideMenu(showSideMenu: boolean) {
-    this.store.dispatch(UiActions.toggleSideMenu({ showSideMenu }));
-  }
-
   getMenuItemKey(index: number, menuItemConfig: MenuItemConfig): string {
     return menuItemConfig.text;
-  }
-
-  getSideMenuMode(mobileMode: boolean): MatDrawerMode {
-    return mobileMode ? 'over' : 'side';
   }
 }
