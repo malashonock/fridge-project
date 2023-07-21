a40661ff65cd7d0581ef2762df4f2f92cff48c7b
Yan Malashonak <yan.malashonak@gmail.com>
2023-07-21 15:07:46 +0300

    fix(state/ui): fix map arg in toggleMobileMode effect

--- src/app/state/ui/ui.effects.ts
+++ src/app/state/ui/ui.effects.ts
@@ -1,23 +1,22 @@
 import { Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
-import { map, withLatestFrom } from 'rxjs';
+import { map } from 'rxjs';
 
 import { UiActions } from './ui.actions';
-import { Store } from '@ngrx/store';
-import { selectMobileMode } from './ui.selectors';
 
 @Injectable()
 export class UiEffects {
-  constructor(private actions$: Actions, private store: Store) {}
+  constructor(private actions$: Actions) {}
 
   toggleMobileMode$ = createEffect(() =>
     this.actions$.pipe(
       ofType(UiActions.toggleMobileMode),
-      withLatestFrom(this.store.select(selectMobileMode)),
-      map(([mobileMode]) =>
+      map(({ mobileMode }) => {
         /* On mobile mode enter, close mobile menu, and vice versa */
-        UiActions.toggleSideMenu({ showSideMenu: mobileMode ? false : true })
-      )
+        return UiActions.toggleSideMenu({
+          showSideMenu: mobileMode ? false : true,
+        });
+      })
     )
   );
 }
