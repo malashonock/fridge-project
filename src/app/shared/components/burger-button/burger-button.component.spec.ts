b439f3912af3764342f309c8af75b16ef853d0cd
Yan Malashonak <yan.malashonak@gmail.com>
2023-07-25 13:14:57 +0300

    test(shared/components): write unit tests for burger-button

--- src/app/shared/components/burger-button/burger-button.component.spec.ts
+++ src/app/shared/components/burger-button/burger-button.component.spec.ts
@@ -1,13 +1,22 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { provideMockStore } from '@ngrx/store/testing';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
+import { HarnessLoader } from '@angular/cdk/testing';
+import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
+import { MatIconHarness } from '@angular/material/icon/testing';
+import { MatButtonHarness } from '@angular/material/button/testing';
 
 import { BurgerButtonComponent } from './burger-button.component';
-import * as uiFeature from 'app/state/ui/ui.feature';
 import { SharedModule } from 'app/shared/shared.module';
+import { selectShowSideMenu } from 'app/state/ui/ui.selectors';
+import { MemoizedSelector } from '@ngrx/store';
+import { UiActions } from 'app/state/ui/ui.actions';
 
 describe('BurgerButtonComponent', () => {
   let component: BurgerButtonComponent;
   let fixture: ComponentFixture<BurgerButtonComponent>;
+  let loader: HarnessLoader;
+  let mockStore: MockStore;
+  let mockSelector: MemoizedSelector<unknown, boolean>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -15,19 +24,51 @@ describe('BurgerButtonComponent', () => {
       imports: [SharedModule],
       providers: [
         provideMockStore({
-          initialState: {
-            ui: uiFeature.initialState,
-          },
+          selectors: [{ selector: selectShowSideMenu, value: true }],
         }),
       ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(BurgerButtonComponent);
     component = fixture.componentInstance;
+    loader = TestbedHarnessEnvironment.loader(fixture);
+    mockStore = TestBed.inject(MockStore);
+    mockSelector = mockStore.overrideSelector(selectShowSideMenu, true);
+
     fixture.detectChanges();
+  });
+
+  afterEach(() => {
+    mockStore.resetSelectors();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should change its appearance depending on show mobile menu flag value', async () => {
+    let icon = await loader.getHarness(MatIconHarness);
+    expect(await icon.getName()).toBe('close');
+
+    mockSelector.setResult(false);
+    mockStore.refreshState();
+
+    // Get icon again - this time it should be a different instance
+    icon = await loader.getHarness(MatIconHarness);
+    expect(await icon.getName()).toBe('menu');
+  });
+
+  it('should dispatch a Toggle Side Menu action on click', async () => {
+    const button = await loader.getHarness(MatButtonHarness);
+    const spyOnStoreDispatch = jest.spyOn(mockStore, 'dispatch');
+
+    await button.click();
+    expect(spyOnStoreDispatch).toHaveBeenCalledTimes(1);
+    expect(spyOnStoreDispatch).toHaveBeenCalledWith(UiActions.toggleSideMenu());
+
+    // And once again
+    await button.click();
+    expect(spyOnStoreDispatch).toHaveBeenCalledTimes(2);
+    expect(spyOnStoreDispatch).toHaveBeenCalledWith(UiActions.toggleSideMenu());
   });
 });
