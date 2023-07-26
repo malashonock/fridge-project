86eb1d56da673f9e55dfd12ba83d2b85a8b48dc3
Yan Malashonak <yan.malashonak@gmail.com>
2023-07-26 12:17:07 +0300

    test(admin/components/admin-index): add missing test dependencies

--- src/app/pages/admin/components/admin-index/admin-index.component.spec.ts
+++ src/app/pages/admin/components/admin-index/admin-index.component.spec.ts
@@ -1,7 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { AdminIndexComponent } from './admin-index.component';
 import { adminPageMenuConfigProvider } from 'app/core/configs/admin-page-menu.config';
+import { SharedModule } from 'app/shared/shared.module';
 
 describe('AdminIndexComponent', () => {
   let component: AdminIndexComponent;
@@ -10,6 +12,7 @@ describe('AdminIndexComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [AdminIndexComponent],
+      imports: [SharedModule, RouterTestingModule],
       providers: [adminPageMenuConfigProvider],
     }).compileComponents();
 
