de0712bec7794a6970e7f82c3c01ec4154e3d009
Yan Malashonak <yan.malashonak@gmail.com>
2023-07-27 17:23:03 +0300

    fix(admin/components/products): add missing test dependencies

--- src/app/pages/admin/components/products/products.component.spec.ts
+++ src/app/pages/admin/components/products/products.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideMockStore } from '@ngrx/store/testing';
 
 import { ProductsComponent } from './products.component';
 
@@ -9,6 +10,7 @@ describe('ProductsComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ProductsComponent],
+      providers: [provideMockStore()],
     }).compileComponents();
 
     fixture = TestBed.createComponent(ProductsComponent);
