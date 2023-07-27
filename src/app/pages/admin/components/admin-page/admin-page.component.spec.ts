159da489438d96d2717e7616db8cd5f6d478995e
Yan Malashonak <yan.malashonak@gmail.com>
2023-07-27 09:19:56 +0300

    test(admin/components): write unit tests for admin-page component

--- src/app/pages/admin/components/admin-page/admin-page.component.spec.ts
+++ src/app/pages/admin/components/admin-page/admin-page.component.spec.ts
@@ -6,31 +6,32 @@ import { provideMockStore } from '@ngrx/store/testing';
 import { AdminPageComponent } from './admin-page.component';
 import * as uiFeature from 'app/state/ui/ui.feature';
 import { SharedModule } from 'shared/shared.module';
+import { adminPageMenuConfigProvider } from 'core/configs';
 
 describe('AdminPageComponent', () => {
-  // let component: AdminPageComponent;
-  // let fixture: ComponentFixture<AdminPageComponent>;
+  let component: AdminPageComponent;
+  let fixture: ComponentFixture<AdminPageComponent>;
 
-  // beforeEach(async () => {
-  //   await TestBed.configureTestingModule({
-  //     declarations: [AdminPageComponent],
-  //     imports: [SharedModule, BrowserAnimationsModule, RouterTestingModule],
-  //     providers: [
-  //       provideMockStore({
-  //         initialState: {
-  //           ui: uiFeature.initialState,
-  //         },
-  //       }),
-  //     ],
-  //   }).compileComponents();
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [AdminPageComponent],
+      imports: [SharedModule, BrowserAnimationsModule, RouterTestingModule],
+      providers: [
+        provideMockStore({
+          initialState: {
+            ui: uiFeature.initialState,
+          },
+        }),
+        adminPageMenuConfigProvider,
+      ],
+    }).compileComponents();
 
-  //   fixture = TestBed.createComponent(AdminPageComponent);
-  //   component = fixture.componentInstance;
-  //   fixture.detectChanges();
-  // });
+    fixture = TestBed.createComponent(AdminPageComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
   it('should create', () => {
-    expect(true).toBe(true);
-    // expect(component).toBeTruthy();
+    expect(component).toBeTruthy();
   });
 });
