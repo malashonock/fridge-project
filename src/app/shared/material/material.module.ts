0cf56dbcb742509a503dca3478d65ffe6264d5eb
Yan Malashonak <yan.malashonak@gmail.com>
2023-07-27 17:24:31 +0300

    fix(admin/components/products): add missing test dependencies

--- src/app/shared/material/material.module.ts
+++ src/app/shared/material/material.module.ts
@@ -9,6 +9,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatTableModule } from '@angular/material/table';
 
 const materialModules = [
   MatFormFieldModule,
@@ -21,6 +22,7 @@ const materialModules = [
   MatMenuModule,
   MatSidenavModule,
   MatTooltipModule,
+  MatTableModule,
 ];
 
 @NgModule({
