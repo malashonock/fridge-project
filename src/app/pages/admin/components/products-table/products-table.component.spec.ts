dc7bb573258dc950bc3d4ee30807daf2ee65640a
Yan Malashonak <yan.malashonak@gmail.com>
2023-07-31 17:19:45 +0300

    test(admin/components): write unit tests for products-table component

--- src/app/pages/admin/components/products-table/products-table.component.spec.ts
+++ src/app/pages/admin/components/products-table/products-table.component.spec.ts
@@ -1,22 +1,155 @@
+import { Component, DebugElement, Input } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatRowHarness } from '@angular/material/table/testing';
+import { HarnessLoader } from '@angular/cdk/testing';
 
 import { ProductsTableComponent } from './products-table.component';
+import { SharedModule } from 'shared/shared.module';
+import { Subject } from 'rxjs';
+import { Product, ProductCategory } from 'core/models';
+import {
+  mockProduct1,
+  mockProducts1,
+  mockProducts2,
+} from 'mocks/product.mocks';
+import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
+import { By } from '@angular/platform-browser';
+
+@Component({
+  selector: 'app-product-details',
+})
+class ProductDetailsStubComponent {
+  @Input() product!: Product;
+}
 
 describe('ProductsTableComponent', () => {
   let component: ProductsTableComponent;
   let fixture: ComponentFixture<ProductsTableComponent>;
+  let loader: HarnessLoader;
+
+  const getMainRows = async (): Promise<MatRowHarness[]> => {
+    return await loader.getAllHarnesses(
+      MatRowHarness.with({ selector: '.main-row' })
+    );
+  };
+
+  const getNoDataRow = (): DebugElement => {
+    return fixture.debugElement.query(By.css('.mat-mdc-no-data-row'));
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ProductsTableComponent],
+      declarations: [ProductsTableComponent, ProductDetailsStubComponent],
+      imports: [SharedModule, NoopAnimationsModule],
     }).compileComponents();
 
     fixture = TestBed.createComponent(ProductsTableComponent);
+    loader = TestbedHarnessEnvironment.loader(fixture);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should mirror changes made to input products observable', () => {
+    const products$ = new Subject<Product[]>();
+    component.products$ = products$;
+    fixture.detectChanges();
+
+    products$.next(mockProducts1);
+    expect(component.dataSource.data).toEqual(mockProducts1);
+
+    products$.next(mockProducts2);
+    expect(component.dataSource.data).toEqual(mockProducts2);
+  });
+
+  it('should mirror changes made to search query observable', () => {
+    const searchQuery$ = new Subject<string>();
+    component.searchQuery$ = searchQuery$;
+    fixture.detectChanges();
+
+    searchQuery$.next('chick');
+    expect(component.dataSource.filter).toEqual('chick');
+
+    searchQuery$.next('');
+    expect(component.dataSource.filter).toEqual('');
+  });
+
+  it('should filter products only by name & ingredients', async () => {
+    const products$ = new Subject<Product[]>();
+    const searchQuery$ = new Subject<string>();
+    component.products$ = products$;
+    component.searchQuery$ = searchQuery$;
+    fixture.detectChanges();
+
+    products$.next(mockProducts2);
+    expect((await getMainRows()).length).toBe(2);
+
+    searchQuery$.next('sandwich');
+    expect((await getMainRows()).length).toBe(1);
+
+    searchQuery$.next('cherry tomatoes');
+    expect((await getMainRows()).length).toBe(1);
+
+    searchQuery$.next(ProductCategory.Salads);
+    expect((await getMainRows()).length).toBe(0);
+    expect(getNoDataRow()).not.toBeNull();
+  });
+
+  describe('sorting data accessor', () => {
+    let sortingDataAccessor: (
+      data: Product,
+      sortHeaderId: string
+    ) => string | number;
+
+    beforeEach(() => {
+      fixture.detectChanges();
+      ({ sortingDataAccessor } = component.dataSource);
+    });
+
+    it('should recognize name, price, category and weight fields', () => {
+      expect(sortingDataAccessor(mockProduct1, 'name')).toBe(mockProduct1.name);
+      expect(sortingDataAccessor(mockProduct1, 'price')).toBe(
+        mockProduct1.price
+      );
+      expect(sortingDataAccessor(mockProduct1, 'category')).toBe(
+        mockProduct1.category
+      );
+      expect(sortingDataAccessor(mockProduct1, 'weight')).toBe(
+        mockProduct1.weight.value
+      );
+    });
+
+    it('should throw an exception for other fields', () => {
+      expect(() => sortingDataAccessor(mockProduct1, 'shelfLife')).toThrowError(
+        'Sorting data accessor not implemented for "shelfLife" column'
+      );
+    });
+  });
+
+  it('should keep reference of the currently expanded row', async () => {
+    const products$ = new Subject<Product[]>();
+    component.products$ = products$;
+    fixture.detectChanges();
+    expect(component.expandedProduct).toBeNull();
+
+    products$.next(mockProducts1);
+    expect(component.expandedProduct).toBeNull();
+
+    // Test via click on the main row
+    fixture.debugElement
+      .query(By.css('.main-row'))
+      .triggerEventHandler('click');
+
+    expect(component.expandedProduct).toBe(mockProduct1);
+
+    // Test via component public API
+    component.toggleExpandProduct(mockProduct1);
+    expect(component.expandedProduct).toBeNull();
+
+    component.toggleExpandProduct(mockProduct1);
+    expect(component.expandedProduct).toBe(mockProduct1);
   });
 });
--- src/app/pages/admin/components/products-table/products-table.component.ts
+++ src/app/pages/admin/components/products-table/products-table.component.ts
@@ -19,7 +19,7 @@ import {
 } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
-import { Observable, Subject, map, takeUntil } from 'rxjs';
+import { Observable, Subject, takeUntil } from 'rxjs';
 
 import { Product } from 'core/models';
 
