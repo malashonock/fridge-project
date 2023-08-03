25cbcca4c28c267617c4865633ccd9d9d43e822d
Yan Malashonak <yan.malashonak@gmail.com>
2023-08-03 12:10:14 +0300

    test(admin/components/product-form): add missing test dependencies

--- src/app/pages/admin/components/product-form/product-form.component.spec.ts
+++ src/app/pages/admin/components/product-form/product-form.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 import { ProductFormComponent } from './product-form.component';
+import { SharedModule } from 'shared/shared.module';
 
 describe('ProductFormComponent', () => {
   let component: ProductFormComponent;
@@ -9,6 +12,8 @@ describe('ProductFormComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ProductFormComponent],
+      imports: [SharedModule, NoopAnimationsModule],
+      providers: [{ provide: MAT_DIALOG_DATA, useValue: undefined }],
     }).compileComponents();
 
     fixture = TestBed.createComponent(ProductFormComponent);
