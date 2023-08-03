8ea3791bddff4c353d496614821b1ccb19a9ab97
Yan Malashonak <yan.malashonak@gmail.com>
2023-08-03 18:19:01 +0300

    feat(shared/components/image-uploader): implement ControlValueAccessor

--- src/app/shared/components/image-uploader/image-uploader.component.ts
+++ src/app/shared/components/image-uploader/image-uploader.component.ts
@@ -4,20 +4,52 @@ import {
   ElementRef,
   Input,
   ViewChild,
+  forwardRef,
 } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 import { FileWithUrl } from 'core/classes';
+
+type ChangeEventHandler = (image: FileWithUrl | null) => void;
 
 @Component({
   selector: 'app-image-uploader',
   templateUrl: './image-uploader.component.html',
   styleUrls: ['./image-uploader.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => ImageUploaderComponent),
+      multi: true,
+    },
+  ],
 })
-export class ImageUploaderComponent {
+export class ImageUploaderComponent implements ControlValueAccessor {
   @Input() public name = '';
 
-  public image: FileWithUrl | null = null;
+  private _image: FileWithUrl | null = null;
+
+  public get image(): FileWithUrl | null {
+    return this._image;
+  }
+
+  public set image(newImage: FileWithUrl | null) {
+    this._image = newImage;
+    this.onChange(newImage);
+
+    if (this.nativeInput) {
+      const dataTransfer = new DataTransfer();
+
+      if (newImage) {
+        dataTransfer.items.add(newImage);
+      }
+
+      this.nativeInput.nativeElement.files = dataTransfer.files;
+    }
+  }
+
+  private onChange: ChangeEventHandler = () => undefined;
 
   @ViewChild('nativeInput') public nativeInput?: ElementRef;
 
@@ -34,10 +66,17 @@ export class ImageUploaderComponent {
 
   public onDiscardFile(): void {
     this.image = null;
+  }
 
-    if (this.nativeInput) {
-      const dataTransfer = new DataTransfer();
-      this.nativeInput.nativeElement.files = dataTransfer.files;
-    }
+  public writeValue(value: FileWithUrl | null): void {
+    this.image = value;
+  }
+
+  public registerOnChange(changeEventHandler: ChangeEventHandler): void {
+    this.onChange = changeEventHandler;
+  }
+
+  public registerOnTouched(): void {
+    // Not used
   }
 }
