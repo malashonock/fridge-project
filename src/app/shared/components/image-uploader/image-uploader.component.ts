0f28ff1cd5470f56d3216e0373ef6ab071cfb7c7
Yan Malashonak <yan.malashonak@gmail.com>
2023-08-04 16:11:49 +0300

    refactor(shared/components/image-uploader): implement MatFormFieldControl interface

--- src/app/shared/components/image-uploader/image-uploader.component.ts
+++ src/app/shared/components/image-uploader/image-uploader.component.ts
@@ -1,15 +1,26 @@
 import {
+  AfterViewInit,
   ChangeDetectionStrategy,
   Component,
   ElementRef,
   Input,
+  OnDestroy,
+  OnInit,
+  Optional,
+  Renderer2,
+  Self,
   ViewChild,
   forwardRef,
 } from '@angular/core';
-import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+import { ControlValueAccessor, FormControl, NgControl } from '@angular/forms';
+import { MatFormFieldControl } from '@angular/material/form-field';
+import { Subject, takeUntil } from 'rxjs';
+import { BooleanInput, coerceBooleanProperty } from '@angular/cdk/coercion';
 
 import { FileWithUrl } from 'core/classes';
+import { FileInputDirective } from 'shared/directives';
 
+type VoidEventHandler = () => void;
 type ChangeEventHandler = (image: FileWithUrl | null) => void;
 
 @Component({
@@ -19,64 +30,179 @@ type ChangeEventHandler = (image: FileWithUrl | null) => void;
   changeDetection: ChangeDetectionStrategy.OnPush,
   providers: [
     {
-      provide: NG_VALUE_ACCESSOR,
+      provide: MatFormFieldControl,
       useExisting: forwardRef(() => ImageUploaderComponent),
-      multi: true,
     },
   ],
 })
-export class ImageUploaderComponent implements ControlValueAccessor {
-  @Input() public name = '';
+export class ImageUploaderComponent
+  implements
+    ControlValueAccessor,
+    MatFormFieldControl<FileWithUrl | null>,
+    OnInit,
+    AfterViewInit,
+    OnDestroy
+{
+  public formControl = new FormControl<FileWithUrl | null>(null, [
+    /* TODO:FileValidator.image */
+  ]);
 
-  private _image: FileWithUrl | null = null;
+  @Input() public id = '';
 
-  public get image(): FileWithUrl | null {
-    return this._image;
+  private _placehoder = 'Select image';
+  public get placeholder(): string {
+    return this._placehoder;
+  }
+  @Input() public set placeholder(value: string) {
+    this._placehoder = value;
+    this.stateChanges.next();
   }
 
-  public set image(newImage: FileWithUrl | null) {
-    this._image = newImage;
-    this.onChange(newImage);
+  public get value(): FileWithUrl | null {
+    return this.formControl.value;
+  }
+  @Input() public set value(value: FileWithUrl | null) {
+    this.formControl.setValue(value);
+  }
 
-    if (this.nativeInput) {
-      const dataTransfer = new DataTransfer();
-
-      if (newImage) {
-        dataTransfer.items.add(newImage);
-      }
-
-      this.nativeInput.nativeElement.files = dataTransfer.files;
+  private _focused = false;
+  public get focused(): boolean {
+    return this._focused;
+  }
+  public set focused(value: boolean) {
+    this._focused = value;
+    if (value === false) {
+      this.touch();
     }
   }
 
-  private onChange: ChangeEventHandler = () => undefined;
+  public get empty(): boolean {
+    return this.value === null;
+  }
+
+  public get shouldLabelFloat(): boolean {
+    return true; // always float
+  }
+
+  private _required = false;
+  public get required(): boolean {
+    return this._required;
+  }
+  @Input() public set required(value: BooleanInput) {
+    this._required = coerceBooleanProperty(value);
+    this.stateChanges.next();
+  }
+
+  public get disabled(): boolean {
+    return this.formControl.disabled;
+  }
+  @Input() public set disabled(value: BooleanInput) {
+    this.setDisabledState(coerceBooleanProperty(value));
+  }
+
+  public get errorState(): boolean {
+    return this.formControl.touched && this.formControl.invalid;
+  }
 
   @ViewChild('nativeInput') public nativeInput?: ElementRef;
+  @ViewChild(FileInputDirective)
+  private fileInputDirective?: FileInputDirective;
+
+  public stateChanges = new Subject<void>();
+  private destroy$ = new Subject();
+
+  public constructor(
+    @Optional() @Self() public ngControl: NgControl,
+    private elementRef: ElementRef,
+    private renderer: Renderer2
+  ) {
+    if (this.ngControl) {
+      this.ngControl.valueAccessor = this;
+    }
+  }
 
   public openFileDialog(): void {
     this.nativeInput?.nativeElement.click();
   }
 
-  public onFileSelected(event: Event): void {
-    const file = (event.target as HTMLInputElement | null)?.files?.[0];
-    if (file) {
-      this.image = new FileWithUrl([file], file.name);
-    }
-  }
-
   public onDiscardFile(): void {
-    this.image = null;
+    this.formControl.reset(null);
   }
 
   public writeValue(value: FileWithUrl | null): void {
-    this.image = value;
+    this.formControl.setValue(value);
   }
 
-  public registerOnChange(changeEventHandler: ChangeEventHandler): void {
-    this.onChange = changeEventHandler;
+  public registerOnChange(onChangeCallback: ChangeEventHandler): void {
+    this.formControl.valueChanges.subscribe(onChangeCallback);
   }
 
-  public registerOnTouched(): void {
-    // Not used
+  private _onTouchedCallback: VoidEventHandler | null = null;
+  public registerOnTouched(onTouchedCallback: VoidEventHandler): void {
+    // FileInputDirective is not available unitl AfterViewInit,
+    // so we need to save the callback temporarily
+    this._onTouchedCallback = () => {
+      onTouchedCallback();
+    };
+  }
+
+  public setDisabledState(isDisabled: boolean): void {
+    isDisabled ? this.formControl.disable() : this.formControl.enable();
+  }
+
+  public touch(): void {
+    if (!this.formControl.touched) {
+      this.formControl.markAsTouched();
+    }
+  }
+
+  public onFocusIn() {
+    if (!this.focused) {
+      this.focused = true;
+      this.stateChanges.next();
+    }
+  }
+
+  public onFocusOut(event: FocusEvent) {
+    if (!this.elementRef.nativeElement.contains(event.relatedTarget)) {
+      this.focused = false;
+      this.stateChanges.next();
+    }
+  }
+
+  public setDescribedByIds(ids: string[]): void {
+    const containerEl =
+      this.elementRef.nativeElement.querySelector('.image-container');
+    this.renderer.setAttribute(containerEl, 'aria-describedby', ids.join(' '));
+  }
+
+  public onContainerClick(event: MouseEvent): void {
+    // Do nothing
+  }
+
+  public ngOnInit(): void {
+    this.formControl.valueChanges
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(() => {
+        this.stateChanges.next();
+      });
+
+    this.formControl.statusChanges
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(() => {
+        this.stateChanges.next();
+      });
+  }
+
+  public ngAfterViewInit(): void {
+    if (this._onTouchedCallback) {
+      this.fileInputDirective?.registerOnTouched(this._onTouchedCallback);
+    }
+  }
+
+  public ngOnDestroy(): void {
+    this.stateChanges.complete();
+    this.destroy$.next(null);
+    this.destroy$.complete();
   }
 }
