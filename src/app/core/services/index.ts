be8af99e736fef27972dbb534a306f860211974a
Yan Malashonak <yan.malashonak@gmail.com>
2023-08-07 11:30:29 +0300

    feat(core/services): implement static-asset service

--- src/app/core/services/index.ts
+++ src/app/core/services/index.ts
@@ -3,3 +3,4 @@ export { LocalStorageService } from './local-storage/local-storage.service';
 export { ProductService } from './product/product.service';
 export { AuthSessionInitializer } from './app-initializer/auth-session.initializer';
 export { ProductsInitializer } from './app-initializer/products.initializer';
+export { StaticAssetService } from './static-asset/static-asset.service';
---
+++ src/app/core/services/static-asset/static-asset.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, map } from 'rxjs';
+
+import { FileWithUrl } from 'core/classes';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class StaticAssetService {
+  public constructor(private httpClient: HttpClient) {}
+
+  public fetchAsset(url: string): Observable<FileWithUrl> {
+    return this.httpClient.get<File>(url).pipe(
+      map((file: File): FileWithUrl => {
+        return new FileWithUrl(file, url);
+      })
+    );
+  }
+}
