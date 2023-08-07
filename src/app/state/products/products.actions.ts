a09e6c59585e4a0f66312851370579352f6f973d
Yan Malashonak <yan.malashonak@gmail.com>
2023-08-07 16:33:47 +0300

    feat(state/products): implement remaining products actions

--- src/app/state/products/products.actions.ts
+++ src/app/state/products/products.actions.ts
@@ -1,12 +1,21 @@
 import { createActionGroup, emptyProps, props } from '@ngrx/store';
 
-import { Product } from 'core/models';
+import { Product, ProductFields } from 'core/models';
 
 export const ProductsActions = createActionGroup({
   source: 'Products',
   events: {
     'Fetch Products': emptyProps(),
     'Fetch Products Success': props<{ products: Product[] }>(),
-    'Fetch Products Error': props<{ error: string }>(),
+    'Fetch Products Failure': props<{ error: string }>(),
+    'Create Product': props<{ productData: ProductFields }>(),
+    'Create Product Success': props<{ product: Product }>(),
+    'Create Product Failure': props<{ error: string }>(),
+    'Update Product': props<{ id: string; productData: ProductFields }>(),
+    'Update Product Success': props<{ product: Product }>(),
+    'Update Product Failure': props<{ error: string }>(),
+    'Delete Product': props<{ id: string }>(),
+    'Delete Product Success': props<{ id: string }>(),
+    'Delete Product Failure': props<{ error: string }>(),
   },
 });
