48791544334ff716e64eb465862594127e6e4917
Yan Malashonak <yan.malashonak@gmail.com>
2023-08-07 16:43:19 +0300

    feat(state/products): implement remaining products reducers

--- src/app/state/products/products.reducers.spec.ts
+++ src/app/state/products/products.reducers.spec.ts
@@ -29,4 +29,90 @@ describe('Products action reducers', () => {
       });
     });
   });
+
+  describe('createProductSuccessReducer', () => {
+    it('should add the created product to store', () => {
+      const originalState: ProductsState = {
+        ids: [],
+        entities: {},
+      };
+
+      const action = ProductsActions.createProductSuccess({
+        product: mockProduct1,
+      });
+
+      const derivedState: ProductsState =
+        ProductsActionReducers.createProductSuccessReducer(
+          originalState,
+          action
+        );
+
+      expect(derivedState).toEqual({
+        ids: [mockProduct1.id],
+        entities: {
+          [mockProduct1.id]: mockProduct1,
+        },
+      });
+    });
+  });
+
+  describe('updateProductSuccessReducer', () => {
+    it('should update the product in store', () => {
+      const originalState: ProductsState = {
+        ids: [mockProduct1.id],
+        entities: {
+          [mockProduct1.id]: mockProduct1,
+        },
+      };
+
+      const action = ProductsActions.updateProductSuccess({
+        product: {
+          ...mockProduct1,
+          name: 'Some other name',
+        },
+      });
+
+      const derivedState: ProductsState =
+        ProductsActionReducers.updateProductSuccessReducer(
+          originalState,
+          action
+        );
+
+      expect(derivedState).toEqual({
+        ids: [mockProduct1.id],
+        entities: {
+          [mockProduct1.id]: {
+            ...mockProduct1,
+            name: 'Some other name',
+          },
+        },
+      });
+    });
+  });
+
+  describe('deleteProductSuccessReducer', () => {
+    it('should delete the product from store', () => {
+      const originalState: ProductsState = {
+        ids: [mockProduct1.id],
+        entities: {
+          [mockProduct1.id]: mockProduct1,
+        },
+      };
+
+      const action = ProductsActions.deleteProductSuccess({
+        id: mockProduct1.id,
+      });
+
+      const derivedState: ProductsState =
+        ProductsActionReducers.deleteProductSuccessReducer(
+          originalState,
+          action
+        );
+
+      expect(derivedState).toEqual({
+        ids: [],
+        entities: {},
+      });
+    });
+  });
 });
--- src/app/state/products/products.reducers.ts
+++ src/app/state/products/products.reducers.ts
@@ -10,6 +10,31 @@ const fetchProductsSuccessReducer: OnReducer<
   return productAdapter.setAll(products, state);
 };
 
+const createProductSuccessReducer: OnReducer<
+  ProductsState,
+  [typeof ProductsActions.createProductSuccess]
+> = (state, { product }) => {
+  return productAdapter.addOne(product, state);
+};
+
+const updateProductSuccessReducer: OnReducer<
+  ProductsState,
+  [typeof ProductsActions.updateProductSuccess]
+> = (state, { product }) => {
+  const { id, ...changes } = product;
+  return productAdapter.updateOne({ id, changes }, state);
+};
+
+const deleteProductSuccessReducer: OnReducer<
+  ProductsState,
+  [typeof ProductsActions.deleteProductSuccess]
+> = (state, { id }) => {
+  return productAdapter.removeOne(id, state);
+};
+
 export const ProductsActionReducers = {
   fetchProductsSuccessReducer,
+  createProductSuccessReducer,
+  updateProductSuccessReducer,
+  deleteProductSuccessReducer,
 };
