3a53027e4cd6624c2bf01d3f836db1373d37acdd
Yan Malashonak <yan.malashonak@gmail.com>
2023-08-16 15:19:23 +0300

    feat(core/models): define fridge interfaces

---
+++ src/app/core/models/fridge/address.interface.ts
@@ -0,0 +1,8 @@
+export interface Address {
+  country: string | null;
+  city: string | null;
+  street: string | null;
+  buildingNo: string | null;
+  floorNo: number | null;
+  roomNo: string | null;
+}
---
+++ src/app/core/models/fridge/product-quantity.interface.ts
@@ -0,0 +1,4 @@
+export interface ProductQuantity {
+  productId: string;
+  quantity: number;
+}
