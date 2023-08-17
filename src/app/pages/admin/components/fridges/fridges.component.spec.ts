ab91e05359826f22d04060318d04be1d278a3ec8
Yan Malashonak <yan.malashonak@gmail.com>
2023-08-17 12:18:11 +0300

    fix(tests): add missing test dependencies

--- src/app/pages/admin/components/fridges/fridges.component.spec.ts
+++ src/app/pages/admin/components/fridges/fridges.component.spec.ts
@@ -1,6 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideMockStore } from '@ngrx/store/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 import { FridgesComponent } from './fridges.component';
+import { SharedModule } from 'shared/shared.module';
+import { FridgesGridComponent } from '../fridges-grid/fridges-grid.component';
 
 describe('FridgesComponent', () => {
   let component: FridgesComponent;
@@ -8,7 +12,9 @@ describe('FridgesComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [FridgesComponent],
+      declarations: [FridgesComponent, FridgesGridComponent],
+      imports: [SharedModule, NoopAnimationsModule],
+      providers: [provideMockStore()],
     }).compileComponents();
 
     fixture = TestBed.createComponent(FridgesComponent);
