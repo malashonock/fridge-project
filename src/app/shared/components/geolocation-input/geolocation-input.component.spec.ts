77eb04126eeb941a9491f0dc1316f0e5a4c0beaf
Yan Malashonak <yan.malashonak@gmail.com>
2023-08-25 17:36:19 +0300

    fix: add missing test dependencies

--- src/app/shared/components/geolocation-input/geolocation-input.component.spec.ts
+++ src/app/shared/components/geolocation-input/geolocation-input.component.spec.ts
@@ -1,6 +1,12 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import '@angular/localize/init';
 
 import { GeolocationInputComponent } from './geolocation-input.component';
+import { MapInputComponent } from '../map-input/map-input.component';
+import { SharedModule } from 'shared/shared.module';
+import { EarlyErrorStateMatcher } from 'core/classes/early-error-state-matcher/early-error-state-matcher.class';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('GeolocationInputComponent', () => {
   let component: GeolocationInputComponent;
@@ -8,7 +14,9 @@ describe('GeolocationInputComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [GeolocationInputComponent],
+      declarations: [GeolocationInputComponent, MapInputComponent],
+      imports: [SharedModule, NoopAnimationsModule, ReactiveFormsModule],
+      providers: [EarlyErrorStateMatcher],
     }).compileComponents();
 
     fixture = TestBed.createComponent(GeolocationInputComponent);
