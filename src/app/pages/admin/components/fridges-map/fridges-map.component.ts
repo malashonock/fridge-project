484235c505b138d8dd459b77b537fe815db19546
Yan Malashonak <yan.malashonak@gmail.com>
2023-08-30 12:03:00 +0300

    feat(admin/components/fridges-map): implement map

--- src/app/pages/admin/components/fridges-map/fridges-map.component.ts
+++ src/app/pages/admin/components/fridges-map/fridges-map.component.ts
@@ -1,4 +1,16 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  OnDestroy,
+  OnInit,
+} from '@angular/core';
+import * as L from 'leaflet';
+
+import { GeolocationCoords } from 'core/models/fridge/geolocation-coords.interface';
+import { Store } from '@ngrx/store';
+import { selectAllFridges } from 'app/state/fridges/fridges.selectors';
+import { Fridge } from 'core/models/fridge/fridge.interface';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-fridges-map',
@@ -6,4 +18,59 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   styleUrls: ['./fridges-map.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class FridgesMapComponent {}
+export class FridgesMapComponent implements OnInit, OnDestroy {
+  private map?: L.Map;
+  private userLocation?: GeolocationCoords;
+  private markers: L.Marker[] = [];
+
+  private destroy$ = new Subject();
+
+  public constructor(private store: Store) {}
+
+  public ngOnInit(): void {
+    this.initMap();
+    this.subscribeMarkersToStoreFridges();
+  }
+
+  public ngOnDestroy(): void {
+    this.destroy$.next(null);
+    this.destroy$.complete();
+  }
+
+  private subscribeMarkersToStoreFridges(): void {
+    this.store
+      .select(selectAllFridges)
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((fridges: Fridge[]): void => {
+        if (!this.map) {
+          return;
+        }
+
+        this.markers = [];
+
+        fridges.forEach((fridge: Fridge): void => {
+          const { latitude, longitude } = fridge.geolocation;
+          const marker = L.marker([latitude, longitude]).addTo(this.map!);
+          this.markers.push(marker);
+        });
+      });
+  }
+
+  private initMap(): void {
+    // Create an empty map
+    this.map = L.map('map', {
+      center: [
+        this.userLocation?.latitude ?? 0,
+        this.userLocation?.longitude ?? 0,
+      ],
+      zoom: this.userLocation ? 16 : 2,
+      minZoom: 2,
+    });
+
+    // Draw the OpenStreetMap layer
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution:
+        '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    }).addTo(this.map);
+  }
+}
