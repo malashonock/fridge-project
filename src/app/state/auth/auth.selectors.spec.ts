b20a33b3f8eea0566eb671d81dd1f95d1aee53bd
Yan Malashonak <yan.malashonak@gmail.com>
2023-09-08 15:31:37 +0300

    refactor(tests): import pre-defined auth mocks from separate module

--- src/app/state/auth/auth.selectors.spec.ts
+++ src/app/state/auth/auth.selectors.spec.ts
@@ -3,40 +3,27 @@ import {
   selectLoggedUser,
   selectSessionExpirationTime,
 } from './auth.selectors';
-import { AuthSession } from './auth.feature';
-import { User } from 'core/models/user/user.interface';
-import { UserRole } from 'core/models/user/user-role.enum';
+import { mockUser } from 'mocks/user.mocks';
+import { mockUserSession } from 'mocks/auth.mocks';
 
 describe('Auth feature selectors', () => {
-  const mockUser: User = {
-    id: '1',
-    name: 'admin',
-    role: UserRole.Admin,
-  };
-  const mockToken = '001@1234567890';
-  const mockExpiresAt = new Date();
-
-  const mockAuthSession: AuthSession = {
-    user: mockUser,
-    token: mockToken,
-    expiresAt: mockExpiresAt,
-  };
-
   describe('selectLoggedUser', () => {
     it('should select user from auth session data', () => {
-      expect(selectLoggedUser.projector(mockAuthSession)).toBe(mockUser);
+      expect(selectLoggedUser.projector(mockUserSession)).toBe(mockUser);
     });
   });
 
   describe('selectAuthToken', () => {
     it('should select access token from auth session data', () => {
-      expect(selectAuthToken.projector(mockAuthSession)).toBe(mockToken);
+      expect(selectAuthToken.projector(mockUserSession)).toBe(
+        mockUserSession.token
+      );
     });
   });
   describe('selectSessionExpirationTime', () => {
     it('should select token expiration date from auth session data', () => {
-      expect(selectSessionExpirationTime.projector(mockAuthSession)).toBe(
-        mockExpiresAt
+      expect(selectSessionExpirationTime.projector(mockUserSession)).toBe(
+        mockUserSession.expiresAt
       );
     });
   });
