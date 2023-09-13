5647d4b1d72034c3b2cccddbeb4d094e7bf421d0
Yan Malashonak <yan.malashonak@gmail.com>
2023-09-13 10:26:29 +0300

    refactor: unload AppModule by moving imports to CoreModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,13 @@
 import { NgModule, isDevMode } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { StoreRouterConnectingModule } from '@ngrx/router-store';
 import { EffectsModule } from '@ngrx/effects';
-import { HttpClientModule } from '@angular/common/http';
 import { LayoutModule } from '@angular/cdk/layout';
 
 import { CoreModule } from 'core/core.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AuthSessionInitializer } from 'core/services/app-initializer/auth-session.initializer';
-import { ProductsInitializer } from 'core/services/app-initializer/products.initializer';
-import { FridgesInitializer } from 'core/services/app-initializer/fridges.initializer';
-import { httpInterceptorProviders } from 'core/interceptors';
 import { authFeature } from './state/auth/auth.feature';
 import { AuthEffects } from './state/auth/auth.effects';
 import { uiFeature } from './state/ui/ui.feature';
@@ -23,16 +16,12 @@ import { productsFeature } from './state/products/products.feature';
 import { ProductsEffects } from './state/products/products.effects';
 import { fridgesFeature } from './state/fridges/fridges.feature';
 import { FridgesEffects } from './state/fridges/fridges.effects';
-import { LeafletInitializer } from 'core/services/app-initializer/leaflet.initializer';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     CoreModule,
-    BrowserModule,
-    BrowserAnimationsModule,
     LayoutModule,
-    HttpClientModule,
     AppRoutingModule,
     StoreModule.forRoot(
       {
@@ -51,13 +40,6 @@ import { LeafletInitializer } from 'core/services/app-initializer/leaflet.initia
       ProductsEffects,
       FridgesEffects,
     ]),
-  ],
-  providers: [
-    AuthSessionInitializer,
-    ProductsInitializer,
-    FridgesInitializer,
-    LeafletInitializer,
-    httpInterceptorProviders,
   ],
   bootstrap: [AppComponent],
 })
