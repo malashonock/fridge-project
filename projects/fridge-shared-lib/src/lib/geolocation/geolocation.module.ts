47ffe002f53356f19e3f4b1d3e8aea9042260433
Yan Malashonak <yan.malashonak@gmail.com>
2023-09-15 14:47:44 +0300

    refactor(shared-lib/components): move geolocation declarables to shared lib module

---
+++ projects/fridge-shared-lib/src/lib/geolocation/geolocation.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
+import { MapInputComponent } from './components/map-input/map-input.component';
+import { GeolocationInputComponent } from './components/geolocation-input/geolocation-input.component';
+import { NumericInputDirective } from '@shared/directives/numeric-input/numeric-input.directive';
+
+const materialModules = [MatFormFieldModule];
+
+@NgModule({
+  declarations: [MapInputComponent, GeolocationInputComponent],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    materialModules,
+    NumericInputDirective,
+  ],
+  exports: [MapInputComponent, GeolocationInputComponent],
+})
+export class GeolocationModule {}
