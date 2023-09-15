4405ca45457cc302b8f8823487cca001dccc7be5
Yan Malashonak <yan.malashonak@gmail.com>
2023-09-15 17:47:44 +0300

    chore(shared-lib): remove scaffolded files

--- projects/fridge-shared-lib/src/lib/fridge-shared-lib.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'lib-fridge-shared-lib',
-  template: ` <p>fridge-shared-lib works!</p> `,
-  styles: [],
-})
-export class FridgeSharedLibComponent {}
--- projects/fridge-shared-lib/src/lib/fridge-shared-lib.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { FridgeSharedLibComponent } from './fridge-shared-lib.component';
-
-@NgModule({
-  declarations: [FridgeSharedLibComponent],
-  imports: [],
-  exports: [FridgeSharedLibComponent],
-})
-export class FridgeSharedLibModule {}
