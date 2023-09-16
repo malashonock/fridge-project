ea0342bc3598ec8a1016a70ff61d4a8fa62b74bf
Yan Malashonak <yan.malashonak@gmail.com>
2023-09-16 23:28:23 +0300

    feat(shared-lib): add BrowserModule to shared CoreModule

--- projects/fridge-shared-lib/src/lib/modules/core/core.module.ts
+++ projects/fridge-shared-lib/src/lib/modules/core/core.module.ts
@@ -1,12 +1,20 @@
 import { ModuleWithProviders, NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { InitializersModule } from './initializers/initializers.module';
 import { httpInterceptorProviders } from './interceptors';
 import { RootStoreModule } from './store/root-store.module';
 
 @NgModule({
-  imports: [RootStoreModule, InitializersModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    RootStoreModule,
+    InitializersModule,
+    HttpClientModule,
+  ],
   providers: [httpInterceptorProviders],
 })
 export class CoreModule {
