fdea32628194d049f4a22ae1f9549289cbd05503
Yan Malashonak <yan.malashonak@gmail.com>
2023-09-22 17:55:00 +0300

    init: migrate workspace to nx integrated monorepo

--- jest.config.js
+++ jest.config.js
@@ -1,26 +1,26 @@
 module.exports = {
-  preset: "jest-preset-angular",
-  setupFilesAfterEnv: ["<rootDir>/setup-jest.ts"],
+  preset: 'jest-preset-angular',
+  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
   moduleDirectories: [
-    "node_modules",
-    "<rootDir>/projects/fridge-shell-app/src",
+    'node_modules',
+    '<rootDir>/apps/fridge-shell-app/src',
   ],
-  transformIgnorePatterns: ["node_modules/(?!@angular|rjxs|@ngrx)"],
+  transformIgnorePatterns: ['node_modules/(?!@angular|rjxs|@ngrx)'],
   collectCoverage: true,
   collectCoverageFrom: [
-    "<rootDir>/projects/fridge-shell-app/src/app/**/*.ts",
-    "!<rootDir>/projects/fridge-shell-app/src/app/**/*.module.ts",
-    "!<rootDir>/projects/fridge-shell-app/src/app/**/index.ts",
-    "!<rootDir>/projects/fridge-shell-app/src/app/**/{mocks,models}/**/*",
+    '<rootDir>/apps/fridge-shell-app/src/app/**/*.ts',
+    '!<rootDir>/apps/fridge-shell-app/src/app/**/*.module.ts',
+    '!<rootDir>/apps/fridge-shell-app/src/app/**/index.ts',
+    '!<rootDir>/apps/fridge-shell-app/src/app/**/{mocks,models}/**/*',
   ],
-  coverageDirectory: "<rootDir>/coverage",
+  coverageDirectory: '<rootDir>/coverage',
   moduleNameMapper: {
-    "^@shell/environments/(.*)": [
-      "<rootDir>/projects/fridge-shell-app/src/environments/$1",
+    '^@shell/environments/(.*)': [
+      '<rootDir>/apps/fridge-shell-app/src/environments/$1',
     ],
-    "^fridge-shared-lib": ["<rootDir>/dist/fridge-shared-lib"],
-    "^@shared/(.*)": ["<rootDir>/projects/fridge-shared-lib/src/lib/$1"],
-    "^@shell/(.*)": ["<rootDir>/projects/fridge-shell-app/src/app/$1"],
-    "^rootDir/(.*)": ["<rootDir>/$1"],
+    '^fridge-shared-lib': ['<rootDir>/dist/fridge-shared-lib'],
+    '^@shared/(.*)': ['<rootDir>/libs/fridge-shared-lib/src/lib/$1'],
+    '^@shell/(.*)': ['<rootDir>/libs/fridge-shell-app/src/app/$1'],
+    '^rootDir/(.*)': ['<rootDir>/$1'],
   },
 };
